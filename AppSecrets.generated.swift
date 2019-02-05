9b1dd6475f1ff47f1a1ed1f2e022f449209ab7f3
Alfredo V <alfredo.villagomez.m@gmail.com>
2019-02-05 10:26:27 -0400

    Delete unsuseless files

--- AppSecrets.generated.swift
+++
@@ -1,8 +0,0 @@
-// Generated using Sourcery 0.15.0 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
-
-
-struct AppSecrets {
-    static let chatApiClientSecret=""
-    static let analyticsWriteKey=""
-}
